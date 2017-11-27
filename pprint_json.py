2d32d08148f774a4c07b60d74bfd2888390b0a0a
dpsmartbusiness <33832856+dpsmartbusiness@users.noreply.github.com>
2017-11-27 17:40:36 +0500

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,16 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    return json.loads(open(filepath, 'r').read())
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = sys.argv[1]
+    pretty_print_json(load_data(sys.argv[1]))
+pass
