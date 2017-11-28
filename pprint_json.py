61db69483548ede96702b364ec931f0a0affabd2
dpsmartbusiness <33832856+dpsmartbusiness@users.noreply.github.com>
2017-11-28 09:33:38 +0500

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -6,8 +6,8 @@ def load_data(filepath):
     return json.loads(open(filepath, 'r').read())
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
+def pretty_print_json(unstructured_data):
+    print(json.dumps(unstructured_data, sort_keys=True, indent=4, ensure_ascii=False, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
